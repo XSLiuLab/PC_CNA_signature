4e0e7d003d6d65ef123f09fc5fbf83a27e32b365
ShixiangWang <wangshx@shanghaitech.edu.cn>
2019-09-17 18:53:00 +0800

    Cache config

--- code/01-calling-cnv-signatures.R
+++ code/01-calling-cnv-signatures.R
@@ -1,4 +1,5 @@
 library(sigminer)
+library(drake)
 library(NMF)
 library(data.table)
 
@@ -19,6 +20,11 @@ make(plan_read, jobs = 2, parallelism = "future")
 # Load data from cache
 loadd(cp_wes, cp_wgs)
 
+hist(cp_wes@data$segVal, breaks = 20)
+hist(cp_wgs@data$segVal, breaks = 20)
+sort(cp_wes@data$segVal, decreasing = TRUE) %>% head(200)
+sort(cp_wgs@data$segVal, decreasing = TRUE) %>% head(200)
+
 # Check distribution
 draw_cn_distribution(cp_wes)
 draw_cn_distribution(cp_wes, mode = "cd", fill = TRUE)
@@ -30,24 +36,44 @@ boxplot(cp_wes@summary.per.sample$cna_burden)
 
 
 # Prepare data and estimate rank -------------------------------------------
+ncores = 8
 
-ncores = 16
+system.time(
+  pre_wes <- derive(cp_wes, cores = ncores, nrep = 3)
+)
+
+system.time(
+  pre_wes2 <- derive(cp_wes, cores = ncores, nrep = 3)
+)
+
 
 #pre_wes = sig_prepare(cp_wes, cores = ncores)
 plan_prepare = drake_plan(
-  pre_wes = sig_prepare(cp_wes, cores = ncores),
-  est_wes = sig_estimate(pre_wes$nmf_matrix,
-                         range = 2:20, nrun = 100, cores = ncores, use_random = TRUE,
-                         save_plots = TRUE, plot_basename = "output/plots/plot_estimate/wes",
-                         verbose = TRUE),
-  pre_wgs = sig_prepare(cp_wgs, reference_components = pre_wes$components, cores = ncores)
+  pre_wes = derive(cp_wes, cores = ncores, nrep = 3),
+  pre_wgs = derive(cp_wgs, reference_components = pre_wes$components, cores = ncores)
 )
+
+system.time(
+  pre_wgs <- sig_prepare(cp_wgs, cores = ncores, nrep = 1)
+)
+
+
+
+save(pre_wes, file = "output/pre_wes.RData")
 
 vis_plan(plan_prepare)
 make(plan_prepare, lock_envir = FALSE)
 
 loadd(pre_wes, pre_wgs)
-#loadd(est_wes)
+
+# est_wes = sig_estimate(pre_wes$nmf_matrix,
+#                        range = 2:15, nrun = 100, cores = ncores, use_random = TRUE,
+#                        save_plots = TRUE, plot_basename = "output/plots/plot_estimate/wes",
+#                        verbose = TRUE),
+# loadd(est_wes)
+
+res = sig_auto_extract(pre_wes$nmf_matrix, result_prefix = "BayesNMF_WES", destdir = "output/BayesNMF/",
+                 nrun = 200, K0 = 20, niter = 2e6, cores = ncores, skip = TRUE)
 
 rank_sury = rbind(est_wes$survey)
 
@@ -59,19 +85,18 @@ par(new = TRUE)
 plot(x = rank_sury$rank, y = rank_sury$rss, type = "b", col = "red", ann = FALSE, axes = FALSE)
 mtext("Error (RSS)", side = 4, line = 3, las = 0, col = "red")
 axis(4)
-# select 5 or 6
+# select 6 or 8
 
 # Choose best signature number
-sigs5 = sig_extract(pre_wes$nmf_matrix, n_sig = 5, nrun = 100, cores = ncores)
-sigs6 = sig_extract(pre_wes$nmf_matrix, n_sig = 6, nrun = 100, cores = ncores)
+sigs = sig_extract(pre_wes$nmf_matrix, n_sig = 8, nrun = 100, cores = ncores)
 
 draw_cn_features(pre_wes$features)
-params = draw_cn_components(pre_wes$features, pre_wes$components)
-draw_sig_profile(sigs5$nmfObj, params = params$parameters, y_expand = 3)
-draw_sig_profile(sigs6$nmfObj, params = params$parameters, y_expand = 3)
+draw_cn_components(pre_wes$features, pre_wgs$components)
 
-draw_cn_features(cn.pre$features)
-draw_cn_components(cn.pre$features, cn.pre$components)
+params = draw_cn_components(pre_wes$features, pre_wes$components)
+draw_sig_profile(sigs$nmfObj, params = params$parameters, y_expand = 3)
+
+
 
 estimate = sig_estimate(cn.pre$nmf_matrix,
                         )
