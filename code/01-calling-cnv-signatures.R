f4ad0573cc547e44347039d210d404d5fd02aa84
ShixiangWang <wangshx@shanghaitech.edu.cn>
2019-09-18 09:32:17 +0800

    call signatures using bayesian NMF

--- code/01-calling-cnv-signatures.R
+++ code/01-calling-cnv-signatures.R
@@ -39,41 +39,26 @@ boxplot(cp_wes@summary.per.sample$cna_burden)
 ncores = 8
 
 system.time(
-  pre_wes <- derive(cp_wes, cores = ncores, nrep = 3)
+  pre_wes.prob <- derive(cp_wes, cores = ncores, nrep = 3)
 )
 
 system.time(
-  pre_wes2 <- derive(cp_wes, cores = ncores, nrep = 3)
+  pre_wes.count <- derive(cp_wes, type = "count", cores = ncores, nrep = 3)
 )
 
+save(pre_wes.prob, file = "output/pre_wes.prob.RData")
+save(pre_wes.count, file = "output/pre_wes.count.RData")
 
-#pre_wes = sig_prepare(cp_wes, cores = ncores)
-plan_prepare = drake_plan(
-  pre_wes = derive(cp_wes, cores = ncores, nrep = 3),
-  pre_wgs = derive(cp_wgs, reference_components = pre_wes$components, cores = ncores)
-)
-
-system.time(
-  pre_wgs <- sig_prepare(cp_wgs, cores = ncores, nrep = 1)
-)
-
-
-
-save(pre_wes, file = "output/pre_wes.RData")
-
-vis_plan(plan_prepare)
-make(plan_prepare, lock_envir = FALSE)
-
-loadd(pre_wes, pre_wgs)
+sig.prob = sig_auto_extract(pre_wes.prob$nmf_matrix, result_prefix = "BayesNMF_Prob",
+                            destdir = "output/signature", cores = 10)
+sig.count = sig_auto_extract(pre_wes.count$nmf_matrix, result_prefix = "BayesNMF_Count",
+                             destdir = "output/signature", cores = 10)
 
 # est_wes = sig_estimate(pre_wes$nmf_matrix,
 #                        range = 2:15, nrun = 100, cores = ncores, use_random = TRUE,
 #                        save_plots = TRUE, plot_basename = "output/plots/plot_estimate/wes",
 #                        verbose = TRUE),
 # loadd(est_wes)
-
-res = sig_auto_extract(pre_wes$nmf_matrix, result_prefix = "BayesNMF_WES", destdir = "output/BayesNMF/",
-                 nrun = 200, K0 = 20, niter = 2e6, cores = ncores, skip = TRUE)
 
 rank_sury = rbind(est_wes$survey)
 
