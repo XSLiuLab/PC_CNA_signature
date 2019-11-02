2c30e3b7b3241a9b8b76f62846747bd0b6c17336
ShixiangWang <wangshx@shanghaitech.edu.cn>
2019-11-02 15:34:52 +0800

    Add exploration of Sig2

--- code/02-data-integration.R
+++ code/02-data-integration.R
@@ -4,20 +4,23 @@ library(sigminer)
 
 load("data/PRAD_CNV.RData")
 Info = readRDS("data/PRAD_CLINICAL.rds")
-CNV.Sig = readRDS("output/NMF_copynumber_signature.prob.rds")
-CNV.Sig.count = readRDS("output/NMF_copynumber_signature.count.rds")
+PurityInfo = read_tsv("data/PRAD_Purity_and_Ploidy_CVAL150.tsv")
 
-# Observe profile
+
+CNV.Sig = readRDS("output/NMF_copynumber_signature.prob.rds")
+#CNV.Sig.count = readRDS("output/NMF_copynumber_signature.count.rds")
+
+# # Observe profile
 load(file = "output/CNV.prob.RData")
 load(file = "output/CNV.count.RData")
+#
+# show_sig_profile(CNV.Sig, params = CNV.prob$parameters, y_expand = 1.5)
+# show_sig_profile(CNV.Sig, params = CNV.prob$parameters, y_expand = 1.5, normalize = 'column')
+# show_sig_profile(CNV.Sig.count, params = CNV.count$parameters, y_expand = 1.5)
+# show_sig_profile(CNV.Sig.count, params = CNV.count$parameters, y_expand = 1.5, normalize = 'column')
 
-show_sig_profile(CNV.Sig, params = CNV.prob$parameters, y_expand = 1.5)
-show_sig_profile(CNV.Sig, params = CNV.prob$parameters, y_expand = 1.5, normalize = 'column')
-show_sig_profile(CNV.Sig.count, params = CNV.prob$parameters, y_expand = 1.5)
-show_sig_profile(CNV.Sig.count, params = CNV.prob$parameters, y_expand = 1.5, normalize = 'column')
 
-
-GroupInfo = get_groups(CNV.Sig, match_consensus = TRUE)
+GroupInfo = get_groups(CNV.Sig, method = 'consensus', match_consensus = TRUE)
 CNVInfo = CNV@summary.per.sample
 ExposureInfo = get_sig_exposure(CNV.Sig)
 
@@ -31,17 +34,19 @@ MergeInfo = Info %>%
          sample_type = ifelse(sample_type == "Unknown", NA_character_, sample_type),
          HasFusion = Fusion,
          HasFusion = ifelse(HasFusion == 'Yes', TRUE, FALSE),
-         IsMetastatic = ifelse(sample_type == 'Metastatic', TRUE, FALSE))
+         IsMetastatic = ifelse(sample_type == 'Metastatic', TRUE, FALSE)) %>%
+  left_join(PurityInfo, by = c('CNV_ID' = 'sample'))
 
 summary(MergeInfo)
 
 cols_to_sigs = paste0('Sig',1:5)
-cols_to_features = c('IsMetastatic', 'HasFusion', 'Age', 'Stage', 'GleasonScore', 'n_of_cnv', 'n_of_amp', 'n_of_del', 'cna_burden')
-feature_type = c(rep('ca', 2), rep('co', 7))
+cols_to_features = c('IsMetastatic', 'HasFusion', 'Age', 'Stage', 'GleasonScore', 'n_of_cnv', 'n_of_amp', 'n_of_del', 'cna_burden', 'purity', 'ploidy')
+feature_type = c(rep('ca', 2), rep('co', 9))
 
 asso_data = get_sig_feature_association(MergeInfo,
                                         cols_to_sigs = cols_to_sigs,
                                         cols_to_features = cols_to_features,
+                                        method_co = "pearson",
                                         type = feature_type, verbose = TRUE)
 # Take a check
 cor(MergeInfo$Sig2, MergeInfo$Sig1, use = 'pairwise.complete.obs', method = 'spearman')
@@ -55,3 +60,31 @@ corrplot::corrplot(asso_data$corr_co$measure,
                    tl.col = "black",
                    p.mat = asso_data$corr_co$p
 )
+
+
+#
+
+Comp_df = CNV.prob$nmf_matrix %>%
+  as.data.frame() %>%
+  tibble::rownames_to_column("sample") %>%
+  as_tibble()
+
+MergeInfo %>%
+  select(CNV_ID, group, enrich_sig, Sig1:Sig5) %>%
+  filter(enrich_sig == 'Sig2') %>%
+  arrange(desc(Sig2)) %>%
+  left_join(Comp_df %>% select(sample, starts_with("bpchrarm")), by = c("CNV_ID"="sample"))
+
+o("/public/data/facet/dbGAP_PLUS_TCGA_PRAD_WES_CVAL150/141-44.pdf")
+
+MergeInfo %>%
+  select(CNV_ID, group, enrich_sig, Sig1:Sig5) %>%
+  filter(enrich_sig == 'Sig2') %>%
+  arrange(desc(Sig2)) %>%
+  left_join(Comp_df %>% select(sample, starts_with("bpchrarm")), by = c("CNV_ID"="sample")) %>%
+  arrange(desc(bpchrarm2))
+
+o("/public/data/facet/dbGAP_PLUS_TCGA_PRAD_WES_CVAL150/909-21020.pdf")
+o("/public/data/facet/dbGAP_PLUS_TCGA_PRAD_WES_CVAL150/554-32.pdf")
+o("/public/data/facet/dbGAP_PLUS_TCGA_PRAD_WES_CVAL150/TCGA-EJ-5515.pdf")
+o("/public/data/facet/dbGAP_PLUS_TCGA_PRAD_WES_CVAL150/TCGA-ZG-A9L5.pdf")
