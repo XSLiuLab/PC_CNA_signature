1ca5e942564ef95a46b5442f48804e7795fe6b13
Shixiang Wang <wangshx@shanghaitech.edu.cn>
2020-03-28 23:13:55 +0800

    Add exposure comparison

---
+++ analysis/src/11-exposure-comparison.R
@@ -0,0 +1,27 @@
+library(sigminer)
+
+load("output/Sig.CNV.seqz.W.RData")
+
+load("output/Sig.CNV.mskcc.RData")
+load("output/CNV.mskcc.tally.W.RData")
+
+# Switch Sig1 and Sig2 to make the signature consistent
+Sig.CNV.mskcc = sig_modify_names(Sig.CNV.mskcc, new_names = paste0("Sig", c(2,1,3:5)))
+
+# expo_mskcc_actual = get_sig_exposure(Sig.CNV.mskcc)
+expo_mskcc_actual = sig_fit(t(CNV.mskcc.tally.W$nmf_matrix), sig = Sig.CNV.mskcc,
+                              mode = "copynumber",
+                              return_class = "data.table")
+
+colnames(expo_mskcc_actual) = c("sample", paste0("MSKCC-Sig", c(2, 1, 3:5)))
+
+expo_mskcc_estimate = sig_fit(t(CNV.mskcc.tally.W$nmf_matrix), sig = Sig.CNV.seqz.W,
+                              mode = "copynumber",
+                              return_class = "data.table")
+
+expo_df <- merge(expo_mskcc_actual, expo_mskcc_estimate, by = "sample", all = TRUE)
+
+pdf(file = "figures/exposure_comparison_with_sig_fit.pdf", width = 6, height = 5.5)
+pheatmap::pheatmap(cor(expo_df[, -1][, c(2,1,3:10)]), display_numbers = TRUE)
+dev.off()
+
